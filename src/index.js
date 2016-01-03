3955959ea0db13df87fa50c456122a9d4d51c299
Jan Dudek <jd@jandudek.com>
2016-01-03 14:53:00 +0100

    Add rendering of the grid

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Cycle from '@cycle/core';
 import { makeDOMDriver, div } from '@cycle/dom';
 import { Observable } from 'rx';
+import _ from 'lodash';
 
 Cycle.run(main, { DOM: makeDOMDriver('#app') });
 
@@ -14,10 +15,66 @@ function intent({ DOM }) {
   return Observable.empty();
 }
 
+// model
+
 function model(action$) {
-  return action$.map((action) => {}).startWith(null);
+  const width = 8, height = 8, count = 10; // beginner
+  // const width = 16, height = 16, count = 40; // intermediate
+  // const width = 30, height = 16, count = 99; // expert
+
+  return action$.
+    startWith(getInitialState(width, height)).
+    do(x => console.log(x));
 }
 
+function getInitialState(width, height) {
+  return {
+    width,
+    height,
+    squares: _.zipObject(
+      allCoords(width, height).map(([x, y]) => [`${x}-${y}`, {
+        x, y,
+      }])
+    ),
+  };
+}
+
+function allCoords(width, height) {
+  return _.flatten(_.times(width, (i) => _.times(height, (j) => [i, j])));
+}
+
+// view
+
 function view(state$) {
-  return state$.map((state) => div(['Hello!']));
+  return state$.map(({ width, height, squares }) =>
+    renderGrid({ width, height, children: _.map(squares, renderSquare) })
+  );
+}
+
+function renderGrid({ width, height, children }) {
+  return div('.grid', {
+    style: {
+      position: 'relative',
+      width: width * SQUARE_WIDTH + 'px',
+      height: height * SQUARE_HEIGHT + 'px',
+      background: '#eee',
+    },
+  }, children);
+}
+
+const SQUARE_WIDTH = 32;
+const SQUARE_HEIGHT = 32;
+
+function renderSquare(square) {
+  return div('.grid__cell', {
+    style: {
+      position: 'absolute',
+      width: SQUARE_WIDTH + 'px',
+      height: SQUARE_HEIGHT + 'px',
+      left: square.x * SQUARE_WIDTH + 'px',
+      top: square.y * SQUARE_HEIGHT + 'px',
+      background: '#eee',
+      border: 'solid 1px #ccc',
+    },
+  });
 }
